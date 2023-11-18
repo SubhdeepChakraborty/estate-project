962827f9cada4e3bc433f51db05895b590c1f5eb
subhdeepchakraborty <chakrabortysubhdeep@icloud.com>
2023-11-18 20:52:32 +0530

    changes/accordin

---
+++ client/src/pages/home/component/Acoordin.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+
+export const data = [
+  {
+    icon: <HiShieldCheck />,
+    heading: "Best interest rates on the market",
+    description:
+      "We provide an affordable rate to the users using this website for their better future",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Secure and Reliable Transactions",
+    description:
+      "Our platform ensures secure and reliable transactions for all users.",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "24/7 Customer Support",
+    description:
+      "Our dedicated team is available round the clock to assist you with any queries.",
+  },
+];
