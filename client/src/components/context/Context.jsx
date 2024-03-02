95c297e072c44afccd38b31eb495e60ebbd08ba3
subhdeepchakraborty <chakrabortysubhdeep@icloud.com>
2024-03-02 13:17:48 +0530

    issue solved and pushed

--- client/src/components/context/Context.jsx
+++ client/src/components/context/Context.jsx
@@ -5,7 +5,7 @@ export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
   const { isOpen, onToggle, onClose } = useDisclosure();
-  const [allow, setAllow] = useState(true);
+  const [allow, setAllow] = useState(false);
   const value = {
     isOpen,
     onToggle,
